bbe6c7f86b12934a4f94f1dc212f2726f2fc58d4
saultoscano <saul.toscano.robles@gmail.com>
2022-05-31 19:55:28 -0700

    Card Change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import Cards from './Cards.jsx'
 
 import profile_pic from '../img/Portfolio_Main_Pic.jpg'
 import weather_app from '../img/Weather_App.png'
+import apiDogs_app from '../img/ApiDogs_App.png'
 
 import '../css/home.css'
 
@@ -19,7 +20,7 @@ function Home(){
                 <hr />
                 <div className='Cards_Container'>
                     <Cards name='Weather App' description='Una aplicacion que te da las tarjetas del clima de la ciudad que le solicites' link='https://SaulToscano.github.io/WeatherApp' picture={weather_app}/>
-                    <Cards name='Api Dogs App' description='Una aplicacion que te da las tarjetas de razas de perros de una api' link='https://SaulToscano.github.io/apiDogs' picture={weather_app}/>
+                    <Cards name='Api Dogs App' description='Una aplicacion que te da las tarjetas de razas de perros de una api' link='https://SaulToscano.github.io/apiDogs' picture={apiDogs_app}/>
                 </div>
 
             </div>
